ffabaeab17039349e7ed232e4a999d10f919b20a
PatrickGTR <patricksubang@live.com>
2019-06-24 22:22:36 +0100

    add error messages

--- task.js
+++ task.js
@@ -1,18 +1,23 @@
 const task_list = [];
 
+
+
 const refreshList = () => {
     $("#todolists").html(""); // clear and ready for appending.
 
     task_list.forEach( (item, index, array) => {
+
+
         // create div element
         $('<div>')
-            .attr({
-                class: `input-group`,
-                id: `itemdiv-${index}`,
-                style: `padding-bottom: 10px;`
-            })
-            .appendTo('#todolists');
+        .attr({
+            class: `input-group`,
+            id: `itemdiv-${index}`,
+            style: `padding-bottom: 10px;`
+        })
+        .appendTo('#todolists');
 
+        const itemdiv = `#itemdiv-${index}`;
         // create input element and attach to div
         $('<input>')
             .attr({
@@ -21,7 +26,7 @@ const refreshList = () => {
                 id: `itemid-${index}`,
                 class: `form-control`,
             })
-            .appendTo(`#itemdiv-${index}`);
+            .appendTo(itemdiv);
 
         // create button element and attach to div
         $('<button>Edit</button>')
@@ -33,7 +38,7 @@ const refreshList = () => {
                 onclick: `editFromList(${index})`,
                 value: `Edit`
             })
-            .appendTo(`#itemdiv-${index}`);
+            .appendTo(itemdiv);
 
         // create button element and attach to div
         $('<button>Delete</button>')
@@ -44,7 +49,7 @@ const refreshList = () => {
                 type: `button`,
                 onclick: `removeFromList(${index})`
             })
-            .appendTo(`#itemdiv-${index}`);
+            .appendTo(itemdiv);
 
     });
 }
@@ -53,7 +58,7 @@ const editFromList = (idx) => {
     const input = $(`#itemid-${idx}`).val();
 
     if(!input) {
-        return;
+        return sendErrorMsg("Edited Task cannot be empty.");
     }
 
     task_list[idx] = input;
@@ -64,7 +69,7 @@ const addToList = () =>  {
     const input = $("#todoInput").val();
 
     if(!input) {
-        return;
+        return sendErrorMsg("Task cannot be empty.");
     }
 
     task_list.push(input);
@@ -79,3 +84,16 @@ const removeFromList = (idx) => {
     task_list.splice(idx, 1);
     refreshList();
 }
+
+const sendErrorMsg = (err_msg) => {
+    $("#error")
+        .attr({
+            style: "padding-top: 10px;"
+        })
+        .html(`<button typegi="button" class="btn btn-danger btn-lg btn-block">${err_msg}</button>`)
+        .fadeIn(250)
+        .delay(2000)
+        .fadeOut(250);
+}
+
+

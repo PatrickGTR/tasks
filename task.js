26474968ad5cca3d3192cc69e788ad882588713f
PatrickGTR <patricksubang@live.com>
2019-06-28 21:49:56 +0100

    track tasks, added double click strike text

--- task.js
+++ task.js
@@ -1,5 +1,11 @@
+const lists = new Set([]);
+
 $(document).on("click", "#btndelete", function() {
     task.remove($(this));
+});
+
+$(document).on("dblclick", "#input", function() {
+    task.complete($(this));
 });
 
 $(document).on("click", "#btnadd", () => task.add($("#todoInput").val()));
@@ -10,18 +16,41 @@ class task {
             return sendErrorMsg("Task cannot be empty.");
         }
 
+        if(lists.has(input)) {
+            return sendErrorMsg("Task already exists.");
+        }
+
         // clear input field
         $("#todoInput").val("");
 
-        $(`<li>${input}<button class="btn-delete" id="btndelete">X</button></li>`)
+        $(
+            `<li><span id="input">${input}</span><button class="btn-delete" id="btndelete">X</button></li>`
+        )
             .attr({
                 "style": "margin-bottom: 10px;",
-                "class": "form-control"
+                "class": "form-control",
             })
             .appendTo("#todolists");
+
+            lists.add(input);
     }
+    static complete(element) {
+        const input = element
+            .parent()
+            .find("span")
+            .text()
+
+        element.html(`<s>${input}</s> COMPLETED!`);
+    }
+
     static remove(element) {
+        const input = element
+            .parent()
+            .find("span")
+            .text();
+
         element.parent().remove();
+        lists.delete(input);
     }
 }
 
@@ -30,7 +59,9 @@ sendErrorMsg = (err_msg) => {
         .attr({
             style: "padding-top: 10px;"
         })
-        .html(`<button type="button" class="btn btn-danger btn-lg btn-block">${err_msg}</button>`)
+        .html(
+            `<button type="button" class="btn btn-danger btn-lg btn-block">${err_msg}</button>`
+        )
         .fadeIn(250)
         .delay(2000)
         .fadeOut(250);
